0af9a5f33e5056d121cdb1cb7d31137fc3407212
BLACKHENZ29 <ayehenz29@gmail.com>
2022-10-14 10:08:46 +0100

    Add project files.

---
+++ Demo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Collection
+{
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList listone = new ArrayList();
+            listone.Add(201);
+            listone.Add("Sophia");
+            listone.Add(" ");
+            listone.Add(true);
+            listone.Add(4.5);
+            listone.Add(null);
+
+            foreach(var item in listone)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+            }
+        }
+    }
+}
